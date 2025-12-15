db21696cfd86b373a1ccb1a3dc607b14c1038ef3
KosiOriginal <kosibgthegamer@gmail.com>
2025-12-15 22:39:22 +0200

    za niki lol

--- find_file.py
+++ find_file.py
@@ -1,15 +1,19 @@
 import os
-found = False
-difficulty = "Easy"
-name_song = "Pekorap Tropica"
+import win32api
+import time
+import win32gui
+import pyautogui
 
+found = False
+difficulty = "Hard" # difuculty
+name_song = "Transform" # imeto na songa
 
 
 
 
 #finding first folder of file
 #os.listdir("") - listing files & directory in directory
-for filename in os.listdir(r"C:\Users\Kosi\AppData\Local\osu!\Songs"):
+for filename in os.listdir(r"C:\Users\Kosi\AppData\Local\osu!\Songs"): # NIKI TOWA E MOQTA DIREKTORIQ . TOEST NQMA DA RABOTI ZA TEB, trqbwa da si slovish svoqta 
     if name_song in filename:
         found = True
         print(filename)
@@ -30,9 +34,30 @@ secound_path = os.path.join(first_path, filename)
 for filename2 in os.listdir(secound_path):
     if difficulty in filename2:
         print(filename2)
+        break
+
+file_path = os.path.join(secound_path, filename2)
+print(file_path)
 
 
 #extracting file data
+with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+    print(f.read())
 
 
+# active windows on screen
+while True:
+    def enum_windows(hwnd, windows):
+        if win32gui.IsWindowVisible(hwnd):
+            title = win32gui.GetWindowText(hwnd)
+            if title:  
+                windows.append(title)
 
+    all_windows = []
+    win32gui.EnumWindows(enum_windows, all_windows)
+
+    #searching osu in active windows
+    for title in all_windows:
+        if "osu" in title.lower():
+            print("Found:", title)
+            time.sleep(0.5)
